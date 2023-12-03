2e3b71436b66ddf4635a28639dd7de407b8f6af9
Vladislav Proskurov <vlad.ghost@gmail.com>
2023-12-03 22:03:42 +0200

    solve day-1 part 2

--- day-1/solve.py
+++ day-1/solve.py
@@ -1,3 +1,27 @@
+WORDS_DICTIONARY = {
+        'one': 1,
+        'two': 2,
+        'three': 3,
+        'four': 4,
+        'five' : 5,
+        'six' : 6,
+        'seven' : 7,
+        'eight' : 8,
+        'nine' : 9,
+    }
+
+def replace_words_with_numbers(lines):
+    new_lines = []
+    for line in lines:
+        new_line = ''
+        for c in line:
+            new_line += c
+            for word in WORDS_DICTIONARY:
+                if word in new_line:
+                    new_line = new_line.replace(word, str(WORDS_DICTIONARY[word]))
+        new_lines.append(new_line)
+    return new_lines
+
 def is_digit(c):
     return '0' <= c <= '9'
 
@@ -19,5 +43,7 @@ def solve(lines):
 
 with open('input', 'r') as f:
     lines = f.readlines()
-    solution = solve(lines)
-    print(solution)
+    solution1 = solve(lines)
+    solution2 = solve(replace_words_with_numbers(lines))
+    print('Solution 1:', solution1)
+    print('Solution 2:', solution2)
